3cd290515fd2a2de73abc7b681f365cfefa8308c
Ian <sebastianvaz123@hotmail.com>
2021-06-11 00:25:36 -0500

    Commit para agregar los archivos nuevos

--- archivos.py
+++ archivos.py
@@ -4,34 +4,33 @@ def read():
     """
     number = []
 
-    with open("./archivos/archivo.txt","r" ,encoding="utf-8" ) as f:
+    with open("./archivos/archivo.txt", "r", encoding="utf-8") as f:
         for line in f:
             number.append(int(line))
-    
+
     print(number)
-        
-    
+
 
 def write():
     """
     esta es la funcion para escribir
     """
-    names = ["Facundo","Cesar","Rocío"]
-    with open("./archivos/names.txt","a") as f:
+    names = ["Facundo", "Cesar", "Rocío"]
+    with open("./archivos/names.txt", "a") as f:
         for name in names:
             f.write(name)
             f.write(" \n")
 
         print()
 
+
 def run():
     """
     esta es la funcion principal 
     """
-    
+
     write()
 
 
 if __name__ == "__main__":
     run()
-
