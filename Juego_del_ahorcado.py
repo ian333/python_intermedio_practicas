83318b1013b626696591fa13b7586e808a0ce39a
Ian <sebastianvaz123@hotmail.com>
2021-06-12 12:54:19 -0500

    "En este commit logramos que aparezcan las imagenes del ahorcado en codigo ascii"

--- Juego_del_ahorcado.py
+++ Juego_del_ahorcado.py
@@ -10,7 +10,13 @@
 # dibuja al ahorcado en cada jugada con codigo ascii
 #
 
-import os
+import os,random,time
+
+
+def presentacion_juego():
+
+    pass
+
 
 def Imagenes_del_ahorcado(intento):
     """
@@ -67,22 +73,36 @@ def Imagenes_del_ahorcado(intento):
       |
 =========''']
 
-    
     os.system("clear")
     print(HANGMANPICS[intento])
 
+def menu_inicial():
+    for i in range(7):
+        time.sleep(0.2)
+        Imagenes_del_ahorcado(i)
+    pass
 
 
 def run():
     """
-    docstring
+    Esta funcion abrira el data.txt 
     """
-    contador_intentos_fallidos=0
-    while contador_intentos_fallidos<=5:
-       
-        letra = int(input("Ingresa un numero"))
+    contador_intentos_fallidos = 0
+    menu_inicial()
+
+    with open("./archivos/data.txt","r", encoding="utf-8") as f:
+        words=[i.replace("\n","") for i in f]
+
+    while contador_intentos_fallidos <= 6:
+        print(words[random.randint(0,len(words))])
+
+        try:
+            letra = int(input("Ingresa un numero"))
+        except ValueError:
+            print("Debes ingresar un numero")
+
         Imagenes_del_ahorcado(contador_intentos_fallidos)
-        contador_intentos_fallidos+=1
+        contador_intentos_fallidos += 1
 
 
 if __name__ == "__main__":
